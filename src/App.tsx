12cfc50842affbd67a740d85d8b81c02123ebedd
Teodora Gjini <teodoragjini@yahoo.com>
2022-08-22 13:51:22 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState()
+
+  return (
+    <div className="App">
+    
+    </div>
+  )
+}
+
+export default App
